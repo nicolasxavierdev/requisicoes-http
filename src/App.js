d80446abbbfdff6306e586102b5783c615205a73
nicolasxavierdev <nicolasxavier.dev@gmail.com>
2021-06-18 08:34:52 -0300

    Requisições HTTP

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import loading from './assets/loading-buffering.gif';
 
 function App() {
+  const [nutri, setNutri] = useState([]);
+  const [error, setError] = useState({ erro: false, msg: '' });
+  const [load, setLoad] = useState(false);
+
+  useEffect(() => {
+
+    function loadApi() {
+      let url = 'https://sujeitoprogramador.com/rn-api/?api=posts';
+
+      fetch(url)
+        .then((r) => r.json())
+        .then((json) => {
+          console.log(json);
+          setNutri(json);
+        }).catch((erro) => {
+          const e = { erro: true, msg: 'Ops, ouve algum erro!' }
+          setError(e);
+          console.error(erro.message)
+        }).finally(() => {
+          setLoad(false);
+        })
+
+    }
+
+    loadApi();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header>
+        <strong>React Nutri</strong>
       </header>
+
+      {load ?
+        <> <img src={loading} alt='loadind...' /> </>
+        :
+        <>
+          {!error.erro ?
+            <>
+              {nutri.map((item) => {
+                return (
+                  <article key={item.id} className="post">
+                    <strong className="titulo">{item.titulo}</strong>
+
+                    <img src={item.capa} alt={item.titulo} className="capa" />
+                    <p className="subtitulo">
+                      {item.subtitulo}
+                    </p>
+                    <a className="botao" href={item.capa} target='_blank'>Acessar</a>
+                  </article>
+                )
+              })}
+
+            </> :
+            <>
+
+              <strong>{error.msg}</strong>
+
+            </>
+          }
+        </>
+      }
+
+
+
     </div>
   );
 }
